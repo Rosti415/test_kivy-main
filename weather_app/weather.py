ab34eae124adfc35ddf5fb3743a1e20e45e9e06f
Rosti415 <116059105+Rosti415@users.noreply.github.com>
2023-07-26 18:29:25 +0300

    add card

--- weather_app/weather.py
+++ weather_app/weather.py
@@ -1,4 +1,6 @@
 from kivy.lang import Builder#підключаємо  в 1 рядку
+from kivymd.uix.card import MDCard
+from kivy.properties import StringProperty
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
@@ -7,6 +9,21 @@ import requests
 from config import*
 
 API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&lang=ua" 
+FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&units=metric&lang=ua"
+
+class WeatherCard(MDCard):
+    def __init__(self, temp, weather_text, wind_speed, rain, rain_pop = None):
+            super().__init__()
+            self.ids.weather_text.text = weather_text.capitalize()
+            self.ids.temp_text.text = "Dg: " + str(round(temp))+ '°C'
+            if rain_pop:
+                self.ids.rain_pop_text.text = f"Ймовірність опадів: {round(rain_pop)* 100}%"
+            self.ids.rain_text.text = f"Кількість опадів: {rain} mm"
+            self.ids.wind_speed_text.text = f"Швидкість вітру: {wind_speed} м/c"
+
+
+        
+
 
 class HomeScreen(MDScreen):
     def __init__(self, **kwargs):
@@ -26,11 +43,11 @@ class HomeScreen(MDScreen):
         weather_data = response['weather'][0]['main']
         desc_data = response['weather'][0]['description']
         wind_data = response['wind']['speed']
-        self.ids.weather_text.text = desc_data.capitalize()
-        self.ids.temp_text.text = "Dg: " + str(round(temp_data))+ '*'
-        self.ids.wind_text.text = "Windy: " + str(round(wind_data)) + 'm/s'
-        print(response)
-        print(temp_data)
+        rain_data = response['rain']['1h']
+        new_card = WeatherCard(temp_data,desc_data,wind_data,rain_data)
+        self.ids.weather_carousel.add_widget(new_card)
+      
+
 
 
 
